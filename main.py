ca26af7beb1643f63ed15abbdef57988e77602ea
Imman <immanuelsavio@gmail.com>
2019-06-05 17:27:43 +0530

    added neural network commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from ParticleSwarmOptimization import ParticleSwarmOptimizedNN
 from utils import train_test_split, to_categorical, normalize, Plot
-#from mlfromscratch.deep_learning import NeuralNetwork
+from NeuralNetwork import NeuralNetwork
 #from mlfromscratch.deep_learning.layers import Activation, Dense
 #from mlfromscratch.deep_learning.loss_functions import CrossEntropy
 #from mlfromscratch.deep_learning.optimizers import Adam
