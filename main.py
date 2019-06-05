0b23b57ce8b2b16fc53f4c54fe9d2d07c2ea95c3
Imman <immanuelsavio@gmail.com>
2019-06-05 17:21:56 +0530

    added files for pso, utils and main program file

---
+++ main.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+from sklearn import datasets
+import matplotlib.pyplot as plt
+import numpy as np
+
+from ParticleSwarmOptimization import ParticleSwarmOptimizedNN
+from utils import train_test_split, to_categorical, normalize, Plot
+#from mlfromscratch.deep_learning import NeuralNetwork
+#from mlfromscratch.deep_learning.layers import Activation, Dense
+#from mlfromscratch.deep_learning.loss_functions import CrossEntropy
+#from mlfromscratch.deep_learning.optimizers import Adam
