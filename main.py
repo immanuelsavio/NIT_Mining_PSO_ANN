4dfbaa3d3336f6b3383d12f8d9619cec6e354648
Imman <immanuelsavio@gmail.com>
2019-06-05 17:41:08 +0530

    added activation functions, layers, loss functions, and optimizers

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ import numpy as np
 from ParticleSwarmOptimization import ParticleSwarmOptimizedNN
 from utils import train_test_split, to_categorical, normalize, Plot
 from NeuralNetwork import NeuralNetwork
-#from mlfromscratch.deep_learning.layers import Activation, Dense
-#from mlfromscratch.deep_learning.loss_functions import CrossEntropy
-#from mlfromscratch.deep_learning.optimizers import Adam
+from layers import Activation, Dense
+from loss_functions import CrossEntropy
+from optimizers import Adam
